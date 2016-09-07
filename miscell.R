5e5d389d50366fd9e1cd1a2fd338adaed691bc33
anupamsingh81 <anupampom@gmail.com>
2016-09-08 03:48:02 +0530

    modified

--- miscell.R
+++ miscell.R
@@ -1,1 +1,13 @@
 # miscellaneous experiment
+# if mistakes in pushing an already existing r directory to git
+# first edit R project file so that it is version control by git instead of none,if no r project file create one in directory.
+# create repository on github
+# 
+# Now we can go in shell of r studio and type following in order
+# 1. git init
+# 2. git add .
+# 3. git commit -m 'message'
+# 4. git remote add origin https://github.com/anupamsingh81/newercode.git replace newer code by name of repository
+# 5. git push -u origin master
+
+# miscell many times step 4 and 5 would be sufficient
